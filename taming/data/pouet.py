fa8f993d639a4f660d49a123c41c3ad62c00c7f4
Lorenz Diener <lorenzd@gmail.com>
2021-04-20 20:17:34 +0000

    fix pouet

--- taming/data/pouet.py
+++ taming/data/pouet.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import math
 import cv2
 import albumentations
 from PIL import Image
@@ -9,7 +10,7 @@ import random
 
 # With semantic map and scene label
 class PouetBase(Dataset):
-    def __init__(self, config=None, size=None, random_crop=False, interpolation="bicubic", crop_size=None, coord=False, val_size=1000):
+    def __init__(self, config=None, size=None, interpolation="bicubic", coord=False, val_size=1000):
         self.split = self.get_split()
         self.coord = coord
         self.rnd = random.Random()
@@ -30,33 +31,6 @@ class PouetBase(Dataset):
             "file_path_": [os.path.abspath(l) for l in self.image_paths],
         }
 
-        size = None if size is not None and size<=0 else size
-        self.size = size
-        if crop_size is None:
-            self.crop_size = size if size is not None else None
-        else:
-            self.crop_size = crop_size
-        if self.size is not None:
-            self.interpolation = interpolation
-            self.interpolation = {
-                "nearest": cv2.INTER_NEAREST,
-                "bilinear": cv2.INTER_LINEAR,
-                "bicubic": cv2.INTER_CUBIC,
-                "area": cv2.INTER_AREA,
-                "lanczos": cv2.INTER_LANCZOS4}[self.interpolation]
-            self.image_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
-                                                                 interpolation=self.interpolation)
-            self.segmentation_rescaler = albumentations.SmallestMaxSize(max_size=self.size,
-                                                                        interpolation=cv2.INTER_NEAREST)
-
-        if crop_size is not None:
-            self.center_crop = not random_crop
-            if self.center_crop:
-                self.cropper = albumentations.CenterCrop(height=self.crop_size, width=self.crop_size)
-            else:
-                self.cropper = albumentations.RandomCrop(height=self.crop_size, width=self.crop_size)
-            self.preprocessor = self.cropper
-
     def __len__(self):
         return self._length
 
@@ -67,14 +41,14 @@ class PouetBase(Dataset):
         if not image.mode == "RGB":
             image = image.convert("RGB")
         image = np.array(image).astype(np.uint8)
+        image = np.pad(
+          image,
+          ((0, math.ceil(image.shape[0]/16) * 16 - image.shape[0]),
+          (0, math.ceil(image.shape[1]/16) * 16 - image.shape[1]),
+          (0, 0))
+        )
 
-        if self.size is not None:
-            image = self.image_rescaler(image=image)["image"]
-      
-        if self.size is not None:
-            processed = self.preprocessor(image=image)
-        else:
-            processed = {"image": image}
+        processed = {"image": image}
         example["image"] = (processed["image"]/127.5 - 1.0).astype(np.float32)
 
         if self.coord == True:
@@ -85,11 +59,6 @@ class PouetBase(Dataset):
 
 
 class PouetTrain(PouetBase):
-    # default to random_crop=True
-    def __init__(self, config=None, size=None, random_crop=True, interpolation="bicubic", crop_size=None, coord=False):
-        super().__init__(config=config, size=size, random_crop=random_crop,
-                          interpolation=interpolation, crop_size=crop_size, coord=coord)
-
     def get_split(self):
         return "train"
 
@@ -101,5 +70,5 @@ class PouetValidation(PouetBase):
 
 if __name__ == "__main__":
     dset = PouetValidation()
-    print(dset[0])
+    print(dset[0]["image"].shape)
     # TODO debug code would go here
